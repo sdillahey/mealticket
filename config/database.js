ef5085ee675cddb96692885a377524599af908d3
Linda Barba <lindabarba13@gmail.com>
2017-05-15 09:54:37 -0700

    fix env configuration

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mealticket');
-const dotenv = require('dotenv');
-dotenv.connect('')
 
 mongoose.Promise = Promise;
 
 var db = mongoose.connection;
-var env = require('env')
-env.connect({
-  host: process.env.mongodb:'//mealticket:mealticket@ds141401.mlab.com:41401/meal-ticket',
-  username: process.env.mealticket,
-  password: process.env.mealticket
-})
 
 db.once('open', function() {
   console.log(`Connected to MongoDB at ${db.host}:{db.port}`);
